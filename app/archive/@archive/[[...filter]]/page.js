9bfefa5c2355640d3b99e28274ef90a8b6c1e405
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-09-20 23:13:38 +0300

    Dealing with multiple path segments

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,58 @@
 import React from "react";
 import Link from "next/link";
-import { getAvailableNewsYears } from "../../../../lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "../../../../lib/news";
+import NewsList from "../../../../components/news-list";
 
 const FilteredNewsPage = ({ params }) => {
   const filter = params.filter;
-  console.log(filter);
-  const links = getAvailableNewsYears();
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the chosen filter!</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
   // return <NewsList news={news} />;
 };
--- components/news-list.js
+++ components/news-list.js
@@ -6,7 +6,7 @@ const NewsList = ({news}) => {
     <ul className="news-list">
       {news.map((newsItem) => (
         <li key={newsItem.id}>
-          <Link href={`/archive/${newsItem.slug}`}>
+          <Link href={`/news/${newsItem.slug}`}>
             <img
               src={`/images/news/${newsItem.image}`}
               alt={newsItem.title}
